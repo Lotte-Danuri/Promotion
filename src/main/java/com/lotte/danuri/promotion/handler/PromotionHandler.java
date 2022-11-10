29265d76c7ec66086958c04d6001e8974e86ac0a
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-11-10 21:32:41 +0900

    FEAN-249 [FEAT] 배포 환경에서의 테스트를 위한 코드 변경

--- src/main/java/com/lotte/danuri/promotion/handler/PromotionHandler.java
+++ src/main/java/com/lotte/danuri/promotion/handler/PromotionHandler.java
@@ -4,6 +4,7 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 
 import com.lotte.danuri.promotion.constant.Promotion;
 import com.lotte.danuri.promotion.redis.RedisService;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -21,10 +22,8 @@ public class PromotionHandler {
     }
 
     public Mono<ServerResponse> addPeople(ServerRequest request) {
-        //String memberId = request.headers().header("memberId").get(0);
+        String memberId = request.headers().header("memberId").get(0);
         //String promotionId = request.headers().header("promotionId").get(0);
-
-        String memberId = "1";
 
         Boolean result = redisService.addPerson(Promotion.PROMOTION.waitKey, memberId);
         if(result == null) {
@@ -35,10 +34,10 @@ public class PromotionHandler {
     }
 
     public Mono<ServerResponse> check(ServerRequest request) {
-        //String memberId = request.headers().header("memberId").get(0);
+        String memberId = request.headers().header("memberId").get(0);
         //String promotionId = request.headers().header("promotionId").get(0);
 
-        String memberId = "1";
+        //String memberId = "1";
         // -1 : 프로모션 종료, null : 대기열 존재 안함, 작업열 존재 -> 쿠폰 받은 거 성공
         Long rank = redisService.getOrderNumber(Promotion.PROMOTION.waitKey, memberId);
         if(rank == null) { // 대기열 존재 안할 때 -> 1. 작업열로 이동, 2. 대기열에 진입 조차 못함
