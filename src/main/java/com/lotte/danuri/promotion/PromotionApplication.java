01bae11b3c48d71f4418fe728bed9bf016f092e2
hellochaeyoung <cyahn0831@hansung.ac.kr>
2022-11-11 09:41:51 +0900

    FEAN-249 [FEAT] 서버 타임존 설정

--- src/main/java/com/lotte/danuri/promotion/PromotionApplication.java
+++ src/main/java/com/lotte/danuri/promotion/PromotionApplication.java
@@ -1,5 +1,7 @@
 package com.lotte.danuri.promotion;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class PromotionApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PromotionApplication.class, args);
